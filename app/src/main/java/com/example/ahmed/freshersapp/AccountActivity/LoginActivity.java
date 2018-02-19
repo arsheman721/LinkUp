cb71714bea8b377f86cfccfd44a2ba42a1849864
arsheman721 <33025465+arsheman721@users.noreply.github.com>
2018-02-19 00:12:30 +0000

    not working logining, will work on next commit

---
+++ app/src/main/java/com/example/ahmed/freshersapp/AccountActivity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.ahmed.freshersapp.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ahmed.freshersapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

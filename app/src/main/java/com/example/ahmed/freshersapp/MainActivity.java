b4cae93c1106f904705687146edc198c433ecce0
Soleimannait <soleiman.nait@gmail.com>
2018-04-25 21:20:47 +0100

    Add onclick listener

--- app/src/main/java/com/example/ahmed/freshersapp/MainActivity.java
+++ app/src/main/java/com/example/ahmed/freshersapp/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.ahmed.freshersapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
